fe17640fcbd8b2843e24513844c3eba25eb78268
Ludek1306 <ludeknavratil84@gmail.com>
2023-04-19 11:53:47 +0200

    Added todo app

---
+++ README.md
@@ -0,0 +1,11 @@
+# Todo-Angular
+
+The Angular CLI ToDo application is a simple web application that allows users to manage their to-do lists. The application is built using Angular Materials and Bootstrap.
+
+With this application, users can easily add, edit, and delete their to-do items. The to-do items can be dragged and dropped from one column to another, making it easy to track their progress. The application is divided into three columns: active to-do, in progress, and finished to-do, giving users a clear picture of their tasks' status.
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.2.1.
+
+## Development server
+
+Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+// angular modules
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+
+// material.io modules
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
+// components
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { TodoComponent } from './todo/todo.component';
+
+@NgModule({
+  declarations: [AppComponent, NavbarComponent, TodoComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    DragDropModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
